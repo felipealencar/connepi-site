2317a22918b0856b39aa115056b450b416fa4cae
Felipe Alencar <felipealencarlopes@gmail.com>
2016-08-29 23:48:45 -0300

    Inserção de read more no texto de apresentação.

--- js/main.js
+++ js/main.js
@@ -105,6 +105,39 @@ $(document).ready(function(){
         }
     });
 
+	// Configure/customize these variables.
+    var showChar = 500;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Leia mais...";
+    var lesstext = "Ocultar";
+    
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 });
 
 // When the window has finished loading create our google map below
