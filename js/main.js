34f64eb9e18913c623faecad30517faffbe6895e
Thiago Lino <devthiagolino@gmail.com>
2016-09-02 10:01:56 -0300

    Deixando a barra fixa

--- js/main.js
+++ js/main.js
@@ -197,13 +197,15 @@ $(document).ready(function(){
 
      
     //animated header class
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 100) {
-            $(".navbar-default").addClass("animated");
-        } else {
-            $(".navbar-default").removeClass('animated');
-        }
-    });
+    if($('.hotsite').length){
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 100) {
+                $(".navbar-default").addClass("animated");
+            } else {
+                $(".navbar-default").removeClass('animated');
+            }
+        });
+    }
 
     $('#countdown_dashboard').countDown({
         targetDate: {
