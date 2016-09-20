2833dc57a581bc4626181961f5db1c29426cc3c9
Anilson Lopes <nissobmx@gmail.com>
2016-09-20 06:14:41 -0300

    Bug fixes

---
+++ manuais-simples/README.md
@@ -0,0 +1,3 @@
+# Manuais
+Navegue pelos diretorios de cada manual,
+o 'index' principal serve apenas para agrupa-los.
---
+++ manuais-simples/js/main.js
@@ -0,0 +1,14 @@
+$(window).load(function(){
+  $('#etapa-0').css('display', 'block');
+})
+
+// steps
+$(document).ready(function(){
+  $('button').on('click', function(){
+    $('.etapa').each(function(){
+      $(this).css('display', 'none');
+      $(this).addClass('animated fadeIn');
+    });
+    $('#' + $(this).val()).css('display', 'block');
+  });
+});
